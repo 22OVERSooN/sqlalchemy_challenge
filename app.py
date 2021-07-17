2f63e76bb338d8ad785ee691227c27ebedb42f62
Antler_Zutter <xiaokunyu@macbook-pro.home>
2021-07-17 15:10:28 +1000

    push py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #In order to analysis the table
 import pandas as pd
+from sqlalchemy.sql.elements import Null
 import numpy as np
 #In order to retrive data from sqlite database
 import datetime as dt
@@ -21,7 +22,7 @@ Base.classes.keys()
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 #create our session from python to the db
-session = Session(engine)
+
 
 app = Flask(__name__)
 
@@ -29,13 +30,57 @@ app = Flask(__name__)
 def homepage():
     return(f"Welcome to weather analysis API in Hawaii<br/>"
         f"Here is all the routes you can use: <br/>"
-        f"/api/v1.0/precipitation"
-        f"/api/v1.0/stations"
-        f"/api/v1.0/tobs"
-        f"/api/v1.0/<start>"
-        f"/api/v1.0/<start>/<end>")
+        f"/api/v1.0/precipitation <br/>"
+        f"/api/v1.0/stations <br/>"
+        f"/api/v1.0/tobs <br/>"
+        f"/api/v1.0/<start> <br/>"
+        f"/api/v1.0/start/end <br/>")
 
+@app.route("preciptation")
+def precipitation():
+    session = Session(engine)
+    one_year_before = dt.date(2017,8,23)-dt.timedelta(days = 365)
+    results = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= one_year_before).\
+            order_by(Measurement.date).all()
+    session.close()
+    precdic = {date: x for date, x in results}
+    return jsonify(precdic)
 
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    results = session.query(Station.station).all()
+    session.close()
+    station_names = list(np.ravel(results))
+    return jsonify(station_names)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+            filter(Measurement.date >= '2017,8,23').all()
+    tobs_list = list(np.ravel(results))
+    session.close()
+    return jsonify(tobs_list)
+
+@app.route("/api/v1.0/start/end")
+def start_end(start, end):
+    session = Session(engine)
+    if end is Null:
+        end == '2017-08-23'
+        results = session.query(Measurement.tobs).\
+            filter(Measurement.date>= start).filter(Measurement.date<= end).all()
+        session.close()
+    else:
+        results = session.query(Measurement.tobs).\
+            filter(Measurement.date>= start).filter(Measurement.date<= end).all()
+        session.close()
+    tobs_list = []
+    for row in results:
+        tobs_list.append(ros.tobs)
+    return(jsonify({"temps_min" : min(tobs_list), "temps_max":max(tobs_list), "temp_avg": np.mean}))
 
 if __name__ == "__main__":
     app.run(debug=True)
